7340d0215d0b3364d6c76eb5ab41a7e0db3e5c99
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-20 13:00:43 +0200

    Added documentation and method to add parameters to querystring. Added ToString() override for getting full Url.

--- SteamWebRequest/SteamWebRequest/UrlBuilder/UrlBuilder.cs
+++ SteamWebRequest/SteamWebRequest/UrlBuilder/UrlBuilder.cs
@@ -17,6 +17,12 @@ namespace SteamWebRequest
         /// querystring (may be added later).
         /// </summary>
         /// <param name="baseUrl">base url</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when baseUrl parameter is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when baseUrl parameter is empty
+        /// </exception>
         public UrlBuilder(string baseUrl)
         {
             if (string.IsNullOrEmpty(baseUrl))
@@ -39,6 +45,12 @@ namespace SteamWebRequest
         /// </summary>
         /// <param name="baseUrl">base url</param>
         /// <param name="queries">params array of QueryParam objects</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when baseUrl parameter is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when baseUrl parameter is empty
+        /// </exception>
         public UrlBuilder(string baseUrl, params QueryParam[] queries)
             : this(baseUrl)
         {
@@ -46,6 +58,27 @@ namespace SteamWebRequest
             {
                 _query[pair.Key] = pair.Value;
             }
+        }
+
+        /// <summary>
+        /// Adds key and value to url querystring.
+        /// </summary>
+        /// <param name="key">parameter key</param>
+        /// <param name="value">parameter value</param>
+        public void AddQueryParameter(string key, string value)
+        {
+            _query[key] = value;
+        }
+
+        /// <summary>
+        /// Returns string representation of complete url
+        /// including possible querystring.
+        /// </summary>
+        /// <returns>complete url</returns>
+        public override string ToString()
+        {
+            _uriBuilder.Query = _query.ToString();
+            return _uriBuilder.ToString();
         }
     }
 }
