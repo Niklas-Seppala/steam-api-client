836bd193a9899ffcd8402dd66928328c1eb85d78
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-20 12:46:53 +0200

    Created QueryParam readonly struct.

---
+++ SteamWebRequest/SteamWebRequest/UrlBuilder/QueryParam.cs
@@ -0,0 +1,14 @@
+﻿namespace SteamWebRequest
+{
+    public readonly struct QueryParam
+    {
+        public string Key { get; }
+        public string Value { get; }
+
+        public QueryParam(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
