33323f58b1fa834dce6c6bb0676e3b4ef59fb70f
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-20 21:13:27 +0200

    Created APIException class.

---
+++ SteamWebRequest/SteamWebRequest/Exceptions/APIException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SteamWebRequest
+{
+    [Serializable]
+    public class APIException : Exception
+    {
+        public string Content { get; set; }
+        public int StatusCode { get; set; }
+        
+        public APIException() { }
+        public APIException(string message) : base(message) { }
+        public APIException(string message, Exception inner) : base(message, inner) { }
+        protected APIException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
