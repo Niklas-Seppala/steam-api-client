012982263e3c3cb0bb576fcedcd90e246e829c1a
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-20 18:30:05 +0200

    Created static class for test project. SecretVariables class loads all private developer information form file (developer keys etc.)

---
+++ SteamWebRequest/SteamWebRequest_Tests/SecretVariables.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+
+namespace SWR
+{
+    internal static class SecretVariables
+    {
+        public static string DevKey { get; } = ReadFromFile("devkey");
+
+        private static string ReadFromFile(string keyword)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines("SECRETS.dat");
+                foreach (string line in lines)
+                {
+                    if (line.Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return line.Split("=")[1];
+                    }
+                }
+                throw new Exception("Keyword not found.");
+            }
+            catch 
+            { 
+                throw; 
+            }
+        }
+    }
+}
