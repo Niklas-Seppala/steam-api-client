be25e6b980a971ad039fc68895108bcf3b970edd
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-25 21:13:21 +0200

    Added leaderboard models

---
+++ SteamWebRequest/SteamApiClient/Models/Dota/Leaderboards/Leaderboard.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Converters;
+
+namespace SteamApiClient.Models
+{
+    public class Leaderboard
+    {
+        [JsonProperty("time_posted")]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime TimePosted { get; set; }
+
+        [JsonProperty("next_scheduled_post_time")]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime Next_ScheduledPostTime { get; set; }
+
+        [JsonProperty("server_time")]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime ServerTime { get; set; }
+
+        [JsonProperty("leaderboard")]
+        public List<RankedPlayer> Players { get; set; }
+    }
+}
---
+++ SteamWebRequest/SteamApiClient/Models/Dota/Leaderboards/RankedPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace SteamApiClient.Models
+{
+    public class RankedPlayer
+    {
+        [JsonProperty("team_id")]
+        public ulong TeamId { get; set; }
+
+        [JsonProperty("team_tag")]
+        public string TeamTag { get; set; }
+
+        public string Sponsor { get; set; }
+
+        public string Country { get; set; }
+
+        public ushort Rank { get; set; }
+
+        public string Name { get; set; }
+    }
+}
