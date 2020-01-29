7edadc5f4a139367e39b0d591d215262fcc3e838
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-01-29 21:05:23 +0200

    Added game mode enum

---
+++ SteamWebRequest/SteamApiClient/Client/DotaExtensions/enums/GameMode.cs
@@ -0,0 +1,27 @@
+﻿namespace SteamApiClient.Dota
+{
+    public enum GameMode
+    {
+        None = 0,
+        AllPick = 1,
+        CaptainsMode = 2,
+        RandomDraft = 3,
+        SingleDraft = 4,
+        AllRandom = 5,
+        Intro = 6,
+        Diretide = 7,
+        ReverseCaptainsMode = 8,
+        TheGreeviling = 9,
+        Tutorial = 10,
+        MidOnly = 11,
+        LeastPlayed = 12,
+        NewPlayerPool = 13,
+        CompediumMatchmaking = 14,
+        CoopVsBots = 15,
+        CaptainsDraft = 16,
+        AbilityDraft = 18,
+        AllRandomDeathMatch = 20,
+        One_V_OneMidOnly = 21,
+        RankedMatchMaking = 23
+    }
+}
