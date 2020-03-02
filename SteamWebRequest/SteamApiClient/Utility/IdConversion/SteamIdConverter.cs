f5e9acca3af7915a1b58f8a9f6a542756207a530
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-03 01:13:04 +0200

    using unsigned integers no longer requires checking for negative input.

--- SteamWebRequest/SteamApiClient/Utility/IdConversion/SteamIdConverter.cs
+++ SteamWebRequest/SteamApiClient/Utility/IdConversion/SteamIdConverter.cs
@@ -6,7 +6,7 @@ namespace SteamApi
     /// Class that provides static functions for
     /// Steam id conversions between 32-bit and 64-bit ids.
     /// </summary>
-    internal static class SteamIdConverter
+    public static class SteamIdConverter
     {
         /// <summary>
         /// Converts 32-bit Steam id to 64-bit version.
@@ -14,13 +14,11 @@ namespace SteamApi
         /// <param name="id32Str">32-bit steam id as a string</param>
         public static string SteamIdTo64(string id32Str)
         {
-            if (int.TryParse(id32Str, out int id32))
+            if (uint.TryParse(id32Str, out uint id32))
             {
-                if (id32 < 0)
-                    throw new ArgumentOutOfRangeException("Id can't be negative.");
                 checked { return (id32 + 76561197960265728).ToString(); }
             }
-            throw new ArgumentException("Given string couldn't be parsed to Int32.");
+            throw new ArgumentException("Given string couldn't be parsed to UInt32.");
         }
 
         /// <summary>
@@ -29,14 +27,12 @@ namespace SteamApi
         /// <param name="id64Str">64-bit Steam id as a string</param>
         public static string SteamIdTo32(string id64Str)
         {
-            long difference = 76561197960265728;
-            if (long.TryParse(id64Str, out long id64))
+            ulong difference = 76561197960265728;
+            if (ulong.TryParse(id64Str, out ulong id64))
             {
-                if (id64 < difference)
-                    throw new ArgumentOutOfRangeException("Id is not valid 64-bit id.");
                 checked { return (id64 - difference).ToString(); }
             }
-            throw new ArgumentException("Given string couldn't be parsed to Int64");
+            throw new ArgumentException("Given string couldn't be parsed to UInt64");
         }
 
         /// <summary>
@@ -45,8 +41,6 @@ namespace SteamApi
         /// <param name="id32">32-bit Steam id</param>
         public static ulong SteamIdTo64(uint id32)
         {
-            if (id32 < 0)
-                throw new ArgumentOutOfRangeException("Id cant be negative");
             checked { return (ulong)id32 + 76561197960265728; }
         }
 
