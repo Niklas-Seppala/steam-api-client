b70d9be93202d6362aeedc48a83f1e02a2d3012e
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-10 23:42:48 +0200

    Moved compressed bit value models to models folder

---
+++ SteamWebRequest/SteamApiClient/Models/Dota/BitValues/BarracksStatus.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace SteamApi.Models.Dota
+{
+    /// <summary>
+    /// Dota 2 barracks status data. True means that building is
+    /// still standing.
+    /// </summary>
+    [Serializable]
+    public class BarracksStatus
+    {
+        /// <summary>
+        /// Bottom ranged barracks
+        /// </summary>
+        public bool BottomRanged { get; }
+
+        /// <summary>
+        /// Bottom melee barracks
+        /// </summary>
+        public bool BottomMelee { get; }
+
+        /// <summary>
+        /// Middle ranged barracks
+        /// </summary>
+        public bool MiddleRanged { get; }
+
+        /// <summary>
+        /// Middle melee barracks
+        /// </summary>
+        public bool MiddleMelee { get; }
+
+        /// <summary>
+        /// Top ranged barracks
+        /// </summary>
+        public bool TopRanged { get; }
+
+        /// <summary>
+        /// Top melee barracks
+        /// </summary>
+        public bool TopMelee { get; }
+
+        /// <summary>
+        /// Instantiates BarracksStatus object from byte.
+        /// </summary>
+        /// <param name="value">Barracks status value compressed to 32-bit integer</param>
+        // ┌─┬───────────── not used.
+        // │ │ ┌─────────── bottom Ranged
+        // │ │ │ ┌───────── bottom Melee
+        // │ │ │ │ ┌─────── middle Ranged
+        // │ │ │ │ │ ┌───── middle Melee
+        // │ │ │ │ │ │ ┌─── top Ranged
+        // │ │ │ │ │ │ │ ┌─ top Melee
+        // 0 0 0 0 0 0 0 0
+        public BarracksStatus(int value)
+        {
+            BitVector32 bits = new BitVector32(value);
+
+            BottomRanged = bits[32];
+            BottomMelee = bits[16];
+            MiddleRanged = bits[8];
+            MiddleMelee = bits[4];
+            TopRanged = bits[2];
+            TopMelee = bits[1];
+        }
+    }
+}
