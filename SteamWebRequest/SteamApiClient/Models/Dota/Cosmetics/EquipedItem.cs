3790a703ddde30bcc014b5184b1ca19cf90288d4
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-11 01:40:54 +0200

    Added documentation

--- SteamWebRequest/SteamApiClient/Models/Dota/Cosmetics/EquipedItem.cs
+++ SteamWebRequest/SteamApiClient/Models/Dota/Cosmetics/EquipedItem.cs
@@ -2,10 +2,20 @@
 
 namespace SteamApi.Models.Dota
 {
+    /// <summary>
+    /// Dota 2 player's equiped item model
+    /// </summary>
     [Serializable]
     public sealed class EquipedItem
     {
+        /// <summary>
+        /// Item index
+        /// </summary>
         public ulong DefIndex { get; set; }
+
+        /// <summary>
+        /// item style
+        /// </summary>
         public uint Style { get; set; }
     }
 }
--- SteamWebRequest/SteamApiClient/Models/Dota/Cosmetics/PlayerInventory.cs
+++ SteamWebRequest/SteamApiClient/Models/Dota/Cosmetics/PlayerInventory.cs
@@ -4,14 +4,26 @@ using System.Collections.Generic;
 
 namespace SteamApi.Models.Dota
 {
+    /// <summary>
+    /// Dota 2 player's cosmetic item inventory
+    /// </summary>
     [Serializable]
     public sealed class PlayerInventory
     {
+        /// <summary>
+        /// Inventory status
+        /// </summary>
         public uint Status { get; set; }
 
+        /// <summary>
+        /// Backpack slots
+        /// </summary>
         [JsonProperty("num_backpack_slots")]
         public uint NumBackpackSlots { get; set; }
 
-        public IReadOnlyList<PlayerItem> Items { get; set; }
+        /// <summary>
+        /// List of dota 2 cosmetic items in player's inventory
+        /// </summary>
+        public IReadOnlyList<CosmeticItem> Items { get; set; }
     }
 }
