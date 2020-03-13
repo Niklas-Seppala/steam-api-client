17ad1c085315a75dec3f25ecd926cbbd3fb48420
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-13 14:33:09 +0200

    Renamed exceptions

--- SteamWebRequest/SteamApiClient/Client/Exceptions/ApiEmptyResponseException.cs
+++ SteamWebRequest/SteamApiClient/Client/Exceptions/ApiEmptyResponseException.cs
@@ -8,16 +8,16 @@ namespace SteamApi
     /// </summary>
     /// <typeparam name="T">Expected response model type</typeparam>
     [Serializable]
-    public class EmptyApiResultException<T> : ApiException
+    public class ApiEmptyResultException<T> : ApiException
     {
         /// <summary>
         /// Type of the Requested model
         /// </summary>
         public Type ResponseModelType { get; } = typeof(T);
 
-        public EmptyApiResultException() { }
-        public EmptyApiResultException(string message) : base(message) { }
-        public EmptyApiResultException(string message, Exception inner) : base(message, inner) { }
-        protected EmptyApiResultException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public ApiEmptyResultException() { }
+        public ApiEmptyResultException(string message) : base(message) { }
+        public ApiEmptyResultException(string message, Exception inner) : base(message, inner) { }
+        protected ApiEmptyResultException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
---
+++ SteamWebRequest/SteamApiClient/Client/Exceptions/ApiPrivateContentException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SteamApi
+{
+    /// <summary>
+    /// Thrown when the Steam user's private information was requested.
+    /// </summary>
+    [Serializable]
+    public class ApiPrivateContentException : ApiException
+    {
+        public ApiPrivateContentException() { }
+        public ApiPrivateContentException(string message) : base(message) { }
+        public ApiPrivateContentException(string message, Exception inner) : base(message, inner) { }
+        protected ApiPrivateContentException(
+          SerializationInfo info,
+          StreamingContext context) : base(info, context) { }
+    }
+}
