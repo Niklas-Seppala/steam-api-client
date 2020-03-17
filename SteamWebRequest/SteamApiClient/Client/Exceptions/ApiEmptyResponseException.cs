da64d5075054801f0b8263ae5f7c694aa959c1d7
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-17 02:16:26 +0200

    Removed type param

--- SteamWebRequest/SteamApiClient/Client/Exceptions/ApiEmptyResponseException.cs
+++ SteamWebRequest/SteamApiClient/Client/Exceptions/ApiEmptyResponseException.cs
@@ -6,15 +6,9 @@ namespace SteamApi
     /// <summary>
     /// Thrown when API response didn't return expected data.
     /// </summary>
-    /// <typeparam name="T">Expected response model type</typeparam>
     [Serializable]
-    public class ApiEmptyResultException<T> : ApiException
+    public class ApiEmptyResultException : ApiException
     {
-        /// <summary>
-        /// Type of the Requested model
-        /// </summary>
-        public Type ResponseModelType { get; } = typeof(T);
-
         public ApiEmptyResultException() { }
         public ApiEmptyResultException(string message) : base(message) { }
         public ApiEmptyResultException(string message, Exception inner) : base(message, inner) { }
