c750eddf9324d16f36b84cfcfed2d16c2cbbff3c
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-03-17 18:24:30 +0200

    Created  dota 2 match skill level enum

---
+++ SteamWebRequest/SteamApiClient/Client/enums/DotaSkillLevel.cs
@@ -0,0 +1,13 @@
+﻿namespace SteamApi
+{
+    /// <summary>
+    /// Dota 2 match skill level
+    /// </summary>
+    public enum DotaSkillLevel
+    {
+        Any = 0,
+        Normal = 1,
+        High = 2,
+        VeryHigh = 3,
+    }
+}
